022e0a70939bb36fe9effac0c4b52375a07bd57a
Benjamin Jones <bjones@galois.com>
2017-08-11 17:30:18 -0700

    Fix type errors in SevenSeg

--- SevenSeg.hs
+++ SevenSeg.hs
@@ -21,23 +21,23 @@ encodeChar 9 = 0b1100111
 encodeChar _ = 0b1111111
 
 multiSevenSeg
-  :: (Signal (Unsigned 4),  -- digit 1
-      Signal (Unsigned 4),  -- digit 2
-      Signal (Unsigned 4),  -- digit 3
-      Signal (Unsigned 4))  -- digit 4
-  -> (Signal (Unsigned 3) {- seg -}, Signal (Unsigned 2) {- sel -})
+  :: Signal (Unsigned 4,  -- digit 1
+             Unsigned 4,  -- digit 2
+             Unsigned 4,  -- digit 3
+             Unsigned 4)  -- digit 4
+  -> Signal (Unsigned 3 {- seg -}, Unsigned 4 {- sel -})
 multiSevenSeg = mealy m7 0
-  where m7 (d1,d2,d3,d4) c = (c+1, (seg, sel))
+  where m7 c (d1,d2,d3,d4) = (c+1, (seg, snd sel))
           where
             _ = c :: Unsigned 18
-            topC = fromIntegral (resize (shiftR c 16))
-            lst = [d1,d2,d3,d4]
+            topC = resize (shiftR c 16) :: Unsigned 2
 
-            seg = encodeChar (lst L.!! topC)
+            seg = encodeChar (fst sel)
 
+            sel :: (Unsigned 4, Unsigned 4)
             sel = case topC of
-                    0 -> 0b1110
-                    1 -> 0b1101
-                    2 -> 0b1011
-                    3 -> 0b0111
-                    _ -> 0b1111
+                    0 -> (d1, 0b1110)
+                    1 -> (d2, 0b1101)
+                    2 -> (d3, 0b1011)
+                    3 -> (d4, 0b0111)
+                    _ -> (d1, 0b1111)
